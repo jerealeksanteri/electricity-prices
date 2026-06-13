caf9eebd53b41080d8a913e71b17696526d64aa6
Jere <jere.niemi@solteq.com>
2026-06-13 13:54:20 +0300

    feat: add Dioxus configuration and Tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "all",
+  content: ["./src/**/*.{rs,html}", "./dist/**/*.html"],
+  theme: { extend: {} },
+  plugins: [],
+};
